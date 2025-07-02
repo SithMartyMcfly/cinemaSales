0695554d86135b831a9a2dc0f8f21ebde1781c4e
Sith_Mcfly <antogutilebron@gmail.com>
2025-07-02 13:02:07 +0200

    añadido mapa sala

--- resources/views/lineup/chooseSeat.blade.php
+++ resources/views/lineup/chooseSeat.blade.php
@@ -17,14 +17,40 @@
     <p class="ml-100">Película: {{ $funcion->film->name }}</p>
     <p>Asientos ocupados: {{ $ocupados }}</p>
     <p>Asientos libres: {{ $funcion->sala->capacity - $ocupados }}</p>
-    <div class="grid grid-cols-10 gap-2 w-200 mx-auto">
-        @for ($i = 1; $i < $asientosTotales; $i++)
-            <img src="{{asset('imagenes/asiento.svg')}}" alt="asiento" class="w-5 h-5">
-            @if ($i%5 == 0)
-                
+    <div class="w-fit mx-auto space-y-2">
+        <?php
+        
+        $columnas = 10;
+        $filas = ceil($asientosTotales / $columnas); 
+       /*  var_dump($filas);
+        die();     */        
+        ?>
+        @for ($fila = 0; $fila < $filas; $fila++)
+        <div class ="flex gap-2 justify-center">
+            @for($col = 0; $col < $columnas; $col++)
+            <?php
+            $asiento = $fila * $columnas + $col + 1;
+            ?>
+            @if ($asiento <= $asientosTotales)
+                <img src="{{asset('imagenes/asiento.svg')}}" alt="asiento {{$asiento}}" class="w-10 h-10 p-1 rounded-sm">  
+            @else
             @endif
+            @endfor
+        </div>
         @endfor
     </div>
 </body>
 
 </html>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        const butacas = document.querySelectorAll('img'); //seleccionamos los elementos
+
+        butacas.forEach(function (butaca) {
+            butaca.addEventListener('click', function(){
+                butaca.classList.toggle('bg-green-300'); //le asignamos evento click y un toggle para que vuelvan a su estado y asignen estado
+            });           
+        });
+    });
+</script>
